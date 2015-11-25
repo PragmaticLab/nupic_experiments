de14d789e34f3b31837a70b746a96b43e318ab0b
Jason Xie <jxieeducation@berkeley.edu>
2015-11-24 20:22:28 -0800

    mapleroyals bug

--- mapleroyals/run.py
+++ mapleroyals/run.py
@@ -37,7 +37,7 @@ while True:
 	})
 	predictionQueue.put(int(result.inferences["multiStepBestPredictions"][1]))
 	prediction = predictionQueue.get()
-	print str(timestamp) + ". predicted: " + str(prediction) + ", actual: " + str(players) + ", queuesize: " + str(n.qsize())
+	print str(timestamp) + ". predicted: " + str(prediction) + ", actual: " + str(players) + ", queuesize: " + str(predictionQueue.qsize())
 	resultWriter.writerow([timestamp, prediction, players])
 	futureWriter.writerow([timestamp, players])
 	if count % 1000 == 0:
