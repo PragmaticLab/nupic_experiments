0b1053d5c479b254a0f56d5d6b2ad3219b911c7f
Jason Xie <jason.xie@tubemogul.com>
2015-11-24 18:42:58 -0800

    mapleroyals basics

---
+++ mapleroyals/scrape_data.py
@@ -0,0 +1,27 @@
+import urllib2
+import csv
+import time
+import datetime
+
+def getPlayersOnline():
+	req = urllib2.Request('http://mapleroyals.com/?page=index')
+	response = urllib2.urlopen(req)
+	the_page = response.read()
+	the_page = the_page[the_page.index("Players Online") + 15:]
+	the_page = the_page[:the_page.index("<br />")]
+	number = int(the_page)
+	return number
+
+if __name__ == "__main__":
+	filename = "lagroyals.csv"
+	print "Generating lagroyal data into %s" % filename
+	fileHandle = open(filename,"w")
+	writer = csv.writer(fileHandle)
+	writer.writerow(["timestamp","players"])
+	writer.writerow(["datetime","int"])
+	writer.writerow(["T",""])
+	for i in range(100):
+		timestamp = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
+		players = getPlayersOnline()
+		writer.writerow([timestamp, players])
+		time.sleep(1)
---
+++ mapleroyals/swarm.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+import csv
+from nupic.frameworks.opf.modelfactory import ModelFactory
+from nupic_output import NuPICFileOutput, NuPICPlotOutput
+from nupic.swarming import permutations_runner
+
+import generate_data
+
+SWARM_CONFIG = {
+  "includedFields": [
+    {
+      "fieldName": "timestamp",
+      "fieldType": "datetime"
+    },
+    {
+      "fieldName": "players",
+      "fieldType": "int",
+      "maxValue": 1000,
+      "minValue": 0
+    }
+  ],
+  "streamDef": {
+    "info": "players",
+    "version": 1,
+    "streams": [
+      {
+        "info": "lagroyals",
+        "source": "file://lagroyals.csv",
+        "columns": [
+          "*"
+        ]
+      }
+    ]
+  },
+  "inferenceType": "TemporalMultiStep",
+  "inferenceArgs": {
+    "predictionSteps": [
+      10
+    ],
+    "predictedField": "players"
+  },
+  "iterationCount": -1,
+  "swarmSize": "medium"
+}
+
+
+def swarm_over_data():
+  return permutations_runner.runWithConfig(SWARM_CONFIG,
+    {'maxWorkers': 8, 'overwrite': True})
+
+def run_swarm():
+  input_file = "lagroyals.csv"
+  model_params = swarm_over_data()
+
+if __name__ == "__main__":
+  run_swarm()
